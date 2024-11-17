eb4261ab293fc1a1111c2effccf50d4a4f5913aa
Anton Khorn <anton.pres@yandex.ru>
2024-11-17 18:28:14 +0300

    название

--- rsbuild.config.ts
+++ rsbuild.config.ts
@@ -1,13 +1,11 @@
-import { defineConfig } from '@rsbuild/core';
 import { pluginReact } from '@rsbuild/plugin-react';
-import Title from 'antd/es/skeleton/Title';
 
 export default {
   html: {
     favicon: './src/shared/images/лого с белым фоном.png',
-    title: 'Alpha Revolver',
+    title: 'Horn', // 'Alpha Revolver'
     meta: {
-      description: 'Alpha Revolver - персональная страница - блог Антона Хорн',
+      description: 'Horn - персональная страница - блог Антона Хорн',
     },
   },
 
